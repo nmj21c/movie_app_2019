2e620b7aef33e8c300056087f22f497cfd25493f
Jonghyun <nmj21c@gmail.com>
2020-01-11 17:23:20 +0900

    무비 추가

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Movie app 2019
+
+## Desc
+- npm create-react-app ${app-name} --typescript (타입스크립트로 만들수 있지만 비추)
+- yarn add prop-types : 값 형식 검증
+- state는 변동되는 데이터를 넣을 것
+- mounting(생겨날때), unmounting(죽을때, 화면이 바뀔때 등), updating(업데이트될때)
+- yarn add axios : fetch 헬퍼
+- yarn add gh-pages : cloud upload
+```
+  github static web 제공
+  프로젝트 네임은 소문자로 입력
+  package.json 에 homepage 항목 추가 ex) "homepage" : "https://nmj21c.github.id/movie_app_2019"
+  URL : https://{github id}.github.io/{repository id} ex) https://nmj21c.github.id/movie_app_2019
+  script >> "deploy": "gh-pages -d build"  // -d directory
+
+```
--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState(curState => ({
+      "isLoading": false,
+      movies
+    }))
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div className="test">
+        {isLoading ? "Loding..." : movies.map(movie => <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.small_cover_image} />)}
+      </div>
+    )
+  };
+
 }
 
 export default App;
